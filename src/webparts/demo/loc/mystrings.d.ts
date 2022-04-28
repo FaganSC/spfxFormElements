6f2157a378fde73b03e388f214fc25cc80657366
Shawn Fagan <sfagan@marathonus.com>
2022-04-28 08:22:26 -0400

    Initial Commit

---
+++ src/webparts/demo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DemoWebPartStrings' {
+  const strings: IDemoWebPartStrings;
+  export = strings;
+}
